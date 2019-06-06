c114e810062e98c4d3c9cb4bb7be716e6a1c2591
8e9one <33422357+8e9one@users.noreply.github.com>
2019-06-06 05:32:53 +0200

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
-  alert('Hello world');
+var repository = [ "Bulbasaur","Ivysaur","Venusaur"];
+for (var repository = 1; repository <= 3; repository++) {
+  document.write(name '\'s (height: ' height ' m)', '(weight: ' weight ' kg)' '(type(s): 'type' )' )
+}
+
+var Bulbasaur = {
+  name: "Bulbasaur",
+  weight: 6.9,
+  height: 0.7,
+  type: ['grass ','poison '];
+};
+
+var Ivysaur = {
+  name: "Ivysaur",
+  weight: 13,
+  height: 1,
+  type: ['grass ','poison '];
+};
+
+var Venusaur = {
+  name: "Venusaur",
+  weight: 100,
+  height: 2,
+  type: ['grass ','poison '];
+};
