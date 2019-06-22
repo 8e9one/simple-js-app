f5368db6570dd4654b026d2433cbb83c5d6ca4a6
8e9one <33422357+8e9one@users.noreply.github.com>
2019-06-23 00:18:33 +0200

    Update scripts.js
    Tweek code mostly syntax err

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,30 @@
 var repository = [ 0, 1, 2];
-for (var i = 0; i <= 3; i++) {
-  document.write(name '\'s (height: ' height ' m,)', '( weight: ' weight ' kg,)' '( type(s): 'type' )' )
-}
 
 repository[0] = {
   name: "Bulbasaur",
   weight: 6.9,
   height: 0.7,
-  type: ['grass ','poison '];
+  type: ['grass ','poison ']
 };
 
 repository [1] = {
   name: "Ivysaur",
   weight: 13,
   height: 1,
-  type: ['grass ','poison '];
+  type: ['grass ','poison ']
 };
 
 repository [2]= {
   name: "Venusaur",
   weight: 100,
   height: 2,
-  type: ['grass ','poison '];
+  type: ['grass ','poison ']
 };
+
+/*for (var i = 0; i<= 2; i++) {
+  document.write(repository[i].name)
+}
+*/
+for (var i = 0; i <= 2; i++) {
+  document.write(repository[i].name + '\'s <br> (height: ' + repository[i].height + ' m,)<br>' + '( weight: ' + repository[i].weight + ' kg,)<br>' + '( type: ' + repository[i].type + ' )<br><br>' )
+}
