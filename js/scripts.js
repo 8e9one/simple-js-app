42ce1953e4d449483d1de4ba6d78b02bee308240
8e9one <33422357+8e9one@users.noreply.github.com>
2019-06-23 00:41:19 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -21,10 +21,7 @@ repository [2]= {
   type: ['grass ','poison ']
 };
 
-/*for (var i = 0; i<= 2; i++) {
-  document.write(repository[i].name)
-}
-*/
+
 for (var i = 0; i <= 2; i++) {
   document.write(repository[i].name + '\'s <br> ( height: ' + repository[i].height + ' m,)<br>' + '( weight: ' + repository[i].weight + ' kg,)<br>' + '( type: ' + repository[i].type + ' )<br><br>' )
 }
