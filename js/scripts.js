be9eae383fa5b676a3b37601e9323951eed99e6a
8e9one <33422357+8e9one@users.noreply.github.com>
2019-06-23 16:31:11 +0200

    Update scripts.js
    Add an if statement in for loop, that will add pokemon who has a height grater than 1.5 meters

--- js/scripts.js
+++ js/scripts.js
@@ -24,4 +24,6 @@ repository [2]= {
 
 for (var i = 0; i <= 2; i++) {
   document.write(repository[i].name + '\'s <br> ( height: ' + repository[i].height + ' m,)<br>' + '( weight: ' + repository[i].weight + ' kg,)<br>' + '( type: ' + repository[i].type + ' )<br><br>' )
+  if (repository[i].height > 1.5) {
+document.write(repository[i].name)
 }
